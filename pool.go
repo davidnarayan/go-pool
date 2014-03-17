8c234214631d7ce7f00bc11f2403cba4828efc14
David Narayan <david_narayan@homedepot.com>
2014-03-17 09:54:57 -0400

    Finally a working version that does not block...

--- pool.go
+++ pool.go
@@ -6,206 +6,70 @@
 package pool
 
 import (
-	"container/list"
 	"sync"
-	"sync/atomic"
-	"time"
-
-	//	"github.com/davidnarayan/go-logging"
-	//"github.com/davecgh/go-spew/spew"
 )
 
 //-----------------------------------------------------------------------------
 
-type Pool struct {
-	MaxWorkers int        // Maximum number of workers (i.e. goroutines) to run
-	jobs       *list.List // List of jobs that need to be run
-
-	in      chan *Job // Channel for incoming work
-	Results chan *Job // Channel for outgoing results
-	wg      *sync.WaitGroup
-	counter int32
-}
-
+// JobFunc represents the user-defined function to do some type of work.
 type JobFunc func(...interface{}) (interface{}, error)
 
+// Job holds everything required for a single unit of work
 type Job struct {
-	F        JobFunc
-	Args     []interface{}
-	Result   interface{}
-	Error    error
-	Interval time.Duration
+	F      JobFunc
+	Args   []interface{}
+	Result interface{}
+	Error  error
 }
 
-//-----------------------------------------------------------------------------
+// Pool manages and distributes work to the workers
+type Pool struct {
+	in  chan *Job
+	out chan *Job
 
-// Create a new worker pool
-func New(workers int) (pool *Pool) {
+	wg *sync.WaitGroup
+}
+
+// NewPool creates a new pool with a fixed number of workers
+func NewPool(size int) *Pool {
 	var wg sync.WaitGroup
-
-	// Channels for managing the work
-	in := make(chan *Job)
-	out := make(chan *Job, workers)
-
-	return &Pool{
-		MaxWorkers: workers,
-		jobs:       list.New(),
-		in:         in,
-		Results:    out,
-		wg:         &wg,
-	}
-}
-
-// Add jobs to the pool
-func (pool *Pool) Add(f JobFunc, args ...interface{}) {
-	job := &Job{
-		F:    f,
-		Args: args,
+	p := &Pool{
+		wg:  &wg,
+		in:  make(chan *Job),
+		out: make(chan *Job),
 	}
 
-	atomic.AddInt32(&pool.counter, 1)
-	pool.in <- job
-}
-
-func (pool *Pool) Run() {
-	// Launch the workers
-	for i := 0; i < pool.MaxWorkers; i++ {
-		pool.wg.Add(1)
-		go worker(pool.in, pool.Results, pool.wg)
-	}
-}
-
-func (pool *Pool) GetResult() (*Job, bool) {
-	if atomic.LoadInt32(&pool.counter) == 0 {
-		return nil, false
+	for i := 0; i < size; i++ {
+		p.wg.Add(1)
+		go p.worker()
 	}
 
-	result := <-pool.Results
-	atomic.AddInt32(&pool.counter, -1)
-
-	return result, true
+	return p
 }
 
-func (pool *Pool) Wait() {
-	close(pool.in)
-	pool.wg.Wait()
-}
-
-func worker(in, out chan *Job, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for job := range in {
-		result, err := job.F(job.Args...)
-		job.Result = result
-		job.Error = err
-		out <- job
-	}
-}
-
-/*
-
-// Run the pool by launching all the workers
-func (pool *Pool) Start() {
-	if pool.started {
-		panic("Pool is already started!")
-	}
-
-	// Launch the workers
-	for i := 0; i < pool.MaxWorkers; i++ {
-		pool.wg.Add(1)
-		go pool.worker()
-	}
-
-	pool.started = true
-}
-
-// Stop the pool (waiting on any workers to finish)
-func (pool *Pool) Stop() {
-	if !pool.started {
-		panic("Pool is already stopped!")
-	}
-
-    logging.Trace("1. ===> %+v", pool.wg)
-	pool.wg.Wait()
-    pool.started = false
-    logging.Trace("%+v", pool.wg)
-    close(pool.Out)
-    close(pool.In)
-}
-
-func (pool *Pool) WaitForJob() (*Job) {
-    logging.Trace("POOL COUNTER IS %d", pool.counter)
-
-    if pool.counter == 0 {
-        return nil
-    }
-
-    pool.counter--
-
-    return <-pool.Out
-}
-
-// Run jobs once. This function is useful when the "supervisor" of the process
-// restarts frequently and the job interval may not be met.
-/*
-func (pool *Pool) RunJobsOnce(delay time.Duration) {
-	var seen = make(map[string]bool)
-
-	for _, job := range pool.Jobs {
-		if _, ok := seen[job.Name]; ok {
-			continue
+// Add submits creates a new job for the pool
+func (p *Pool) Add(fn JobFunc, args ...interface{}) {
+	go func() {
+		job := &Job{
+			F:    fn,
+			Args: args,
 		}
 
-		go func(d time.Duration) {
-			logging.Info("Scheduling job: name=%s delay=%s", job.Name, d)
-
-			for {
-				select {
-				case <-time.After(d):
-					pool.In <- job
-					return
-				}
-			}
-		}(delay)
-
-		seen[job.Name] = true
-	}
-}
-*/
-/*
-
-// Run puts all jobs into the incoming queue
-func (pool *Pool) Run() {
-	for _, job := range pool.Jobs {
-        logging.Trace("adding job: %+v", job)
-		pool.In <- job
-	}
+		p.in <- job
+	}()
 }
 
-// Run puts jobs into the incoming queue for the specified interval
-func (pool *Pool) RunAt(interval time.Duration) {
-	for _, job := range pool.Jobs {
-		if job.Interval == interval {
-			logging.Trace("Matched interval job.Interval=%s interval=%s",
-				job.Interval, interval)
-			pool.In <- job
-		} else {
-			logging.Trace("Did not match interval job.Interval=%s interval=%s",
-				job.Interval, interval)
-		}
-	}
+// Return one result
+func (p *Pool) Result() *Job {
+	return <-p.out
 }
 
-// worker runs jobs in the incoming queue
-func (pool *Pool) worker() {
-    //defer pool.wg.Done()
-
-	for job := range pool.In {
-		result, err := job.F(job.Args...)
-		job.Result = result
-		job.Error = err
-		pool.Out <- job
-        pool.wg.Done()
+// worker runs the next job available
+func (p *Pool) worker() {
+	defer p.wg.Done()
+	for {
+		job := <-p.in
+		job.Result, job.Error = job.F(job.Args...)
+		p.out <- job
 	}
 }
-*/
